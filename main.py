9dbb38fe8a4c7e6be5aaaf6ba4522bd6f050207c
davidovicheugne <zh.zhekariks@gmail.com>
2022-03-22 23:11:21 +0300

    +vk and github authorization

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 import time
 import requests
 import os
+import json
+from selenium.webdriver.common.by import By
 
 
 options = webdriver.ChromeOptions()
@@ -11,30 +13,68 @@ driver = webdriver.Chrome(
     options=options,
 )
 
+with open('data.json', 'r') as file:
+    datajson = json.load(file)
 
-def authvk():
-    pass
+with open('accounts.json', 'r') as file:
+    accountjson = json.load(file)
+
+def setdata(source, account, login_field_id, pass_field_id, btn_class, flogin_btn_xpath='clear'):
+    if source == 'gmail':
+        login_input = driver.find_element(By.ID,login_field_id)
+    else:
+        login_input = driver.find_element(By.ID,login_field_id)
+    login_input.send_keys(datajson[source][account]['login'])
+    # if source == 'gmail':
+    #     flogin_btn = driver.find_element(By.XPATH,flogin_btn_xpath)
+    #     flogin_btn.click()
+    pass_input = driver.find_element(By.ID,pass_field_id)
+    pass_input.send_keys(datajson[source][account]['pass'])
+    login_btn = driver.find_element(By.CLASS_NAME, btn_class)
+    login_btn.click()
+    
+def loadpage(page):
+    url = page
+    driver.get(url=url)
+
+def authvk(account):
+    loadpage("https://vk.com/")
+    setdata("vk.com", account, "index_email", "index_pass", "index_login_button")
+
+def authyandex(account):
+    loadpage("")
+    setdata("vk.com", account, "index_email", "index_pass", "index_login_button")
+    time.sleep(10)
 
 
-def authyandex():
-    pass
+def authgmail(account):
+    url = "https://accounts.google.com/AccountChooser?service=mail&continue=https://mail.google.com/mail/"
+    driver.get(url=url)
+    setdata("gmail", account, "identifierId", "index_pass", "index_login_button",
+        flogin_btn="/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button"
+    )
+    time.sleep(10)
 
 
-def authgmail():
-    pass
+# def authnotion(account):
+#     loadpage("")
+#     setdata("vk.com", account, "index_email", "index_pass", "index_login_button")
+#     time.sleep(10)
 
 
-def authnotion():
-    pass
-
-
-def authgithub():
-    pass
+def authgithub(account):
+    loadpage("https://github.com/login")
+    setdata("github", account, "login_field", "password", "js-sign-in-button")
+    
 
 
 def main():
     try:
-        pass
+        authvk(accountjson['vk'])
+        authgithub(accountjson['github'])
+        # authgmail(accountjson['gmail'])
+
+        time.sleep(100)
     except Exception as ex:
         print(ex)
     finally:
